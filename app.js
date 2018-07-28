ac8a1ed51889601656bb4f1be8be57d851b157f9
Eugene <emosby@bham.rr.com>
2018-07-28 14:53:24 -0500

    Unpdated files

---
+++ app.js
@@ -0,0 +1,98 @@
+/*
+let name = "Eugene Mosby";
+const numOfStates = 50;
+let sum = 5 + 4;
+
+//Convert first letter of name to ASCII Code
+let firstLetter = "E";
+let standard = "L";
+let compare = standard.charCodeAt(0);
+let result = firstLetter.charCodeAt(0);
+
+if (result > compare) {
+    alert("Back of the line");
+} else {
+    alert("Next");
+}
+
+let sayHello = function () {
+    alert("Hello World");
+};
+sayHello();
+
+//let checkAge = function (name, age) {
+    //if (age < 21) {
+        //alert(`Sorry ${name} you aren't old enough to view this page.`);
+    //}
+//};
+//checkAge("Charles", 21);
+//checkAge("Abby", 27);
+//checkAge("James", 18);
+//checkAge("John", 17);
+
+let veggie = ["Okra", "Corn", "Beets", "Collar Greens", "Potatoes"];
+for (let i = 0; i < veggie.length; i++) {
+    console.log(veggie[i]);
+}
+*/
+
+/*
+let checkAge = [{
+    name: "John", age: 27,
+}, {
+    name: "Mary", age: 20,
+}, {
+    name: "Luke", age: 19,
+}, {
+    name: "Peter", age: 36,
+}, {
+    name: "Lulu", age: 25}
+    ];
+let students = checkAge.map(function (students) {
+    return students;
+});
+console.log(students);
+*/
+
+let getLength = function (word) {
+    return word.length;
+};
+let result = getLength("Hello Worlds");
+//console.log(result);
+if (result % 2 === 0) {
+   console.log("Number is even");
+} else {
+    console.log("Number is odd")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
